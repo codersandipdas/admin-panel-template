51a01a097f3a4790663f4d4ce094cbb906b76ff6
Sandip Das <codersandipdas@gmail.com>
2024-12-16 23:16:53 +0530

    improved auth logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import { Route, Routes, useNavigate } from 'react-router';
+import { Navigate, Route, Routes, useNavigate } from 'react-router';
 import './App.css';
 import MainLayout from './layouts/MainLayout';
 import { routes } from './utils/routes';
 import SignIn from './pages/signIn/SignIn';
 import { useEffect, useState } from 'react';
 
+const AuthenticatedRoute = ({ children }: { children: JSX.Element }) => {
+  const token = localStorage.getItem('token');
+  return token ? children : <Navigate to='/signin' />;
+};
+
 function App() {
   const navigate = useNavigate();
 
   const [isLoading, setIsLoading] = useState(true);
-  const [auth, setAuth] = useState<any>(null);
 
   useEffect(() => {
     (async () => {
       try {
         const token = localStorage.getItem('token');
         if (!token) {
-          navigate('/signin');
           return;
         }
-
-        setAuth({ token });
       } catch (err) {
         console.log('err', err);
       } finally {
@@ -35,20 +36,20 @@ function App() {
 
   return (
     <Routes>
-      {auth ? (
-        <Route path='/' element={<MainLayout />}>
-          {routes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={route.component}
-            />
-          ))}
-        </Route>
-      ) : (
-        <Route path='/signin' element={<SignIn />} />
-      )}
+      <Route
+        path='/'
+        element={
+          <AuthenticatedRoute>
+            <MainLayout />
+          </AuthenticatedRoute>
+        }
+      >
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.component} />
+        ))}
+      </Route>
 
+      <Route path='/signin' element={<SignIn />} />
       <Route path='*' element={<div>404</div>} />
     </Routes>
   );
